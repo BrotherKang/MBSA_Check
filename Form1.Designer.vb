3ee591d947b28412e2bee09f1b02122344354fec
BrotherKang <neofancy.epaper@gmail.com>
2023-05-25 15:27:04 +0800

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,110 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        txtSource = New TextBox()
+        Label1 = New Label()
+        txtTarget = New TextBox()
+        Label2 = New Label()
+        btnRun = New Button()
+        txtKB_Installed = New TextBox()
+        SuspendLayout()
+        ' 
+        ' txtSource
+        ' 
+        txtSource.BorderStyle = BorderStyle.FixedSingle
+        txtSource.Location = New Point(12, 48)
+        txtSource.Name = "txtSource"
+        txtSource.Size = New Size(356, 23)
+        txtSource.TabIndex = 0
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(12, 30)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(55, 15)
+        Label1.TabIndex = 1
+        Label1.Text = "來源路徑"
+        ' 
+        ' txtTarget
+        ' 
+        txtTarget.BorderStyle = BorderStyle.FixedSingle
+        txtTarget.Location = New Point(12, 115)
+        txtTarget.Name = "txtTarget"
+        txtTarget.Size = New Size(356, 23)
+        txtTarget.TabIndex = 2
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(15, 89)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(55, 15)
+        Label2.TabIndex = 3
+        Label2.Text = "目的路徑"
+        ' 
+        ' btnRun
+        ' 
+        btnRun.Location = New Point(140, 171)
+        btnRun.Name = "btnRun"
+        btnRun.Size = New Size(81, 31)
+        btnRun.TabIndex = 4
+        btnRun.Text = "執行"
+        btnRun.UseVisualStyleBackColor = True
+        ' 
+        ' txtKB_Installed
+        ' 
+        txtKB_Installed.Location = New Point(7, 208)
+        txtKB_Installed.Multiline = True
+        txtKB_Installed.Name = "txtKB_Installed"
+        txtKB_Installed.ScrollBars = ScrollBars.Both
+        txtKB_Installed.Size = New Size(364, 378)
+        txtKB_Installed.TabIndex = 5
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(383, 598)
+        Controls.Add(txtKB_Installed)
+        Controls.Add(btnRun)
+        Controls.Add(Label2)
+        Controls.Add(txtTarget)
+        Controls.Add(Label1)
+        Controls.Add(txtSource)
+        FormBorderStyle = FormBorderStyle.FixedSingle
+        MaximizeBox = False
+        Name = "Form1"
+        Text = "MBSA資料篩選"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents txtSource As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtTarget As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnRun As Button
+    Friend WithEvents txtKB_Installed As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,123 @@
+﻿Imports System.IO
+Imports System.Xml
+
+Public Class Form1
+
+    Private Sub btnRun_Click(sender As Object, e As EventArgs) Handles btnRun.Click
+
+        ' 遍尋路徑下所有目錄及檔案
+        Dim strSource As New System.IO.DirectoryInfo(txtSource.Text) '設定路徑變數並實作
+        Dim strMbsaXml As System.IO.FileInfo    ' 宣告檔案名稱變數
+        Dim subDir As System.IO.DirectoryInfo   ' 宣告子目錄變數
+
+        '清除路徑頭尾的空白
+        txtTarget.Text = Trim(txtTarget.Text)
+
+        '建立結果XML檔
+        '如果目的路徑為空值，則使用來源路徑
+        If txtTarget.Text = "" Then
+            txtTarget.Text = txtSource.Text
+        End If
+
+        '判斷來源目的後面是否有"\"
+        If txtTarget.Text.EndsWith("\") = False Then
+            txtTarget.Text += "\"
+        End If
+
+        Dim strDataTime As String = DateTime.Now.ToString("yyyyMMddHHmmss") '取目前的日期時間作為檔名的一部份，避免檔名重複
+
+        Dim strXmlFile As String = txtTarget.Text + "MBSA_" + strDataTime + ".xml"  ' 設定檔案名稱(含路徑)
+        Dim strCsvFile As String = txtTarget.Text + "MBSA_" + strDataTime + ".csv"
+
+        Dim fileMbsaXml As System.IO.StreamWriter
+
+        fileMbsaXml = My.Computer.FileSystem.OpenTextFileWriter(strXmlFile, True)
+        fileMbsaXml.WriteLine("<XMLOut>")  '寫入ROOT節點
+        fileMbsaXml.Close()
+
+        Dim fileMbsaCsv As System.IO.StreamWriter   ' 新增CSV檔案部分
+        fileMbsaCsv = My.Computer.FileSystem.OpenTextFileWriter(strCsvFile, True)   ' 新增CSV檔案部分
+        fileMbsaCsv.WriteLine("NampIP,UpdateData")  ' 新增CSV檔案部分
+
+        ' 設定暫存檢查結果變數
+        Dim tmpTXT
+
+        '尋找所有子目錄
+        For Each subDir In strSource.GetDirectories
+
+            '取出子目錄的名稱(去除"_REPORT"，以作為主機名稱辨識
+            txtKB_Installed.Text = txtKB_Installed.Text + subDir.Name.Replace("_REPORT", "") + vbCrLf
+
+            '搜尋子目錄下所有的XML檔案
+            For Each strMbsaXml In subDir.GetFiles("*.xml")
+                Dim rdMBSAXML As XmlReader = XmlReader.Create(strMbsaXml.FullName) '讀取MBSA XML檔案
+                tmpTXT = "" ' 每次新的檔案就將暫存變數清空
+
+                Do While rdMBSAXML.Read()  '讀取XML中所有節點
+                    '將MBSA XML中所有 UpData 元素讀取出來，以判斷是否安裝及等級
+                    '元素 UpData 中，是否安裝屬性為第5個(由0開始計算)
+                    ' KB 編號屬性為第3個，等級為第6個
+                    If rdMBSAXML.NodeType = XmlNodeType.Element AndAlso rdMBSAXML.Name = "UpdateData" Then
+                        '判斷更新是否安裝，且等級大於等於3
+                        If rdMBSAXML.GetAttribute(5) = "false" And rdMBSAXML.GetAttribute(6) >= 3 Then
+                            ' tmpTXT = rdMBSAXML.GetAttribute(3) + " / " + rdMBSAXML.GetAttribute(5) + " / " + rdMBSAXML.GetAttribute(6) + vbCrLf
+                            tmpTXT += "KB" + rdMBSAXML.GetAttribute(3) + vbCrLf
+                        End If
+                    End If
+                Loop
+                rdMBSAXML.Close() '關閉XML檔案
+                '將有結果的部分放入預覽框中
+                txtKB_Installed.Text = txtKB_Installed.Text + tmpTXT + "------------" + vbCrLf
+
+                '將結果寫入檔案
+                fileMbsaXml = My.Computer.FileSystem.OpenTextFileWriter(strXmlFile, True)
+                fileMbsaXml.WriteLine("<MBSA_Check>")  '包住每個主機的結果
+                fileMbsaXml.WriteLine("  <NameIp>" + subDir.Name.Replace("_REPORT", "") + "</NameIp>") '寫入主機名稱(含IP)
+
+                Dim tmpCsvTXT ' 因應新增CSV檔案部分，增加此暫存變數存放CSV內容
+
+                ' 判斷tmpTXT有無資料，決定寫入檔案內容
+                If tmpTXT = "" Then
+                    fileMbsaXml.WriteLine("  <UpdataData>OS(含OFFICE更新) : 無嚴重度3級以上未更新</UpdataData>")
+                    tmpCsvTXT = "OS(含OFFICE更新) : 無嚴重度3級以上未更新"   ' 新增CSV檔案部分
+                Else
+                    fileMbsaXml.WriteLine("    <UpdataData> OS(含OFFICE更新):" + vbCrLf + tmpTXT + "未完成更新</UpdataData>")
+                    tmpCsvTXT = """OS(含OFFICE更新):" + vbCrLf + tmpTXT + "未完成更新"""    ' 新增CSV檔案部分
+                End If
+                fileMbsaXml.WriteLine("</MBSA_Check>")
+                fileMbsaXml.Close()
+
+                fileMbsaCsv.WriteLine(subDir.Name.Replace("_REPORT", "") + "," + tmpCsvTXT) ' 新增CSV檔案部分
+            Next
+
+        Next
+
+        fileMbsaXml = My.Computer.FileSystem.OpenTextFileWriter(strXmlFile, True)
+        fileMbsaXml.WriteLine("</XMLOut>")
+        fileMbsaXml.Close()
+
+        fileMbsaCsv.Close() ' 新增CSV檔案部分
+
+
+
+
+        'Dim strMBSAXML As XmlReader = XmlReader.Create(txtSource.Text) '讀取MBSA XML檔案
+        'Do While strMBSAXML.Read()  '讀取XML中所有節點
+        '    Dim tmpTXT
+        '    '將MBSA XML中所有 UpData 元素讀取出來，以判斷是否安裝及等級
+        '    '元素 UpData 中，是否安裝屬性為第5個(由0開始計算)
+        '    ' KB 編號屬性為第3個，等級為第6個
+        '    If strMBSAXML.NodeType = XmlNodeType.Element AndAlso strMBSAXML.Name = "UpdateData" Then
+        '        '判斷更新是否安裝，且等級大於等於3
+        '        If strMBSAXML.GetAttribute(5) = "false" And strMBSAXML.GetAttribute(6) >= 3 Then
+        '            tmpTXT = strMBSAXML.GetAttribute(3) + " / " + strMBSAXML.GetAttribute(5) + " / " + strMBSAXML.GetAttribute(6) + vbCrLf
+        '            txtKB_Installed.Text = txtKB_Installed.Text + tmpTXT
+        '        End If
+
+        '    End If
+        'Loop
+
+        MessageBox.Show("OK")
+
+    End Sub
+End Class
